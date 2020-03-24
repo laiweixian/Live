1ed3ac5c31718527a888cd6c4f51e94af3c57e20
laiweixian <lwxmiku@163.co,>
2020-03-24 17:50:02 +0800

    小更新

--- LiveServerForRtmp/Rtmp/RtmpApp/RtmpRoom/RtmpClient/RtmpMessages/Commands/NetConnection/Connect.h
+++ LiveServerForRtmp/Rtmp/RtmpApp/RtmpRoom/RtmpClient/RtmpMessages/Commands/NetConnection/Connect.h
@@ -4,33 +4,8 @@
 #include "../../AMF/AMF3.h"
 #include "stdafx.h"
 
-struct connectCommandObject
-{
-	string app;
-	string flashver;
-	string swfUrl;
-	string tcUrl;
-	bool fpad;
-	uint32_t audioCodecs;
-	uint32_t videoCodecs;
-	uint32_t videoFunction;
-	string pageUrl;
-	uint32_t objectEncoding;
-};
-
-struct connectOptionalUserArguments
-{
-
-};
-
-struct connectCommand
-{
-	string name;
-	uint64_t transactionID;
-
-};
-
 class CConnect
 {
+	
 
 };
